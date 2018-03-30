4bc0d94039bfe9ebfaaf1797f36795e444b8bb1c
zp672087110 <672087110@qq.com>
2018-03-31 02:26:15 +0800

    Add files via upload

---
+++ logistic.py
@@ -0,0 +1,124 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+
+#% matplotlib inline
+
+#  we will perform logistic regression using  a simple  toy data set of two  classes
+
+X ,y_true = make_blobs(n_samples = 1000,centers= 2)
+
+fig = plt.figure(figsize =(8,6))
+plt.scatter(X[:,0],X[:,1],c = y_true)
+
+plt.title("datasets")
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+plt.show()
+
+
+# reshape  targets to get column vector with shaple (n_samples,1)
+y_true = y_true[:,np.newaxis]
+
+# split the  data  into a  training set  and test set
+
+X_train,X_test,y_train,y_test = train_test_split(X,y_true)
+
+print(f"shape X_train: {X_train.shape}")
+
+print(f"shape y_train:{y_train.shape}")
+print(f"shape X_test:{X_test.shape}")
+print(f"shape y_test: {y_test.shape}")
+
+
+
+# logistic regression
+
+class LogisticcRegression:
+	"""docstring for LogisticcRegression"""
+	def __init__(self):
+		pass
+
+	def sigmoid(self,a):
+		return 1/(1+ np.exp(-a))
+
+	def train(self,X,y_true,n_iters,learning_rate):
+		"""
+		trains the logistic regression model on given data X and targets y
+
+		"""
+		# step 0 Initialize the parameters
+		n_samples,n_features = X.shape
+		self.weights = np.zeros((n_features,1))
+		self.bias = 0
+		costs = []
+
+		for i in range(n_iters):
+			# step1 and 2 :compute a linear combination of the input features and weights
+			# apply the sigmod activation function
+
+			y_predict = self.sigmoid(np.dot(X,self.weights)+self.bias)
+
+			# step 3 compute the cost over  the whole training set
+			cost = (-1/n_samples) * np.sum(y_true*np.log(y_predict) +(1-y_true)*(np.log(1-y_predict)))
+
+			# step 4 compute the gradient
+			dw = (1/n_samples) * np.dot(X.T,(y_predict - y_true))
+			db = (1/n_samples) * np.sum(y_predict - y_true)
+
+			# step 5 update the parameters
+
+			self.weights = self.weights - learning_rate * dw
+			self.bias = self.bias- learning_rate*db
+
+			costs.append(cost)
+			if i %100 ==0:
+				print(f"cost  after the iteration {i}:{cost}")
+
+
+		return self.weights, self.bias,costs
+
+	def predict(self,X):
+		"""
+		Predict binary labels for a set of example X
+		"""
+
+		y_predict = self.sigmoid(np.dot(X,self.weights) + self.bias)
+
+		y_predict_label = [1 if elem >0.5 else 0 for elem in y_predict]
+
+		return np.array(y_predict_label)[:,np.newaxis]
+
+
+#  initialize the model
+
+regressor = LogisticcRegression()
+
+w_trained, b_trained ,costs = regressor.train(X_train,
+											 y_train,
+											 n_iters =800,
+											 learning_rate=0.009)
+
+fig = plt.figure(figsize=(8,6))
+plt.plot(np.arange(800),costs)
+plt.title("development of cost over training")
+plt.xlabel("number of iterations")
+plt.ylabel("cost")
+plt.show()
+
+
+# test
+
+print("begin to test the model")
+
+y_p_train = regressor.predict(X_train)
+y_p_test = regressor.predict(X_test)
+print(f"train accuracy :{100 - np.mean(np.abs(y_p_train-y_train))*100}%")
+print(f"test accuracy :{100 - np.mean(np.abs(y_p_test-y_test))*100}%")
+
+
+
+		
